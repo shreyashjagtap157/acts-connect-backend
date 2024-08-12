500da892467387337276996cfd5382f13b59a4ef
anurag <anuragch198@gmail.com>
2024-08-12 17:03:11 +0530

    Added dependency for dotenv and set up application-dev.properties and application-prod.properties References Issue #10

--- src/main/java/com/connect/acts/ActsConnectBackend/ActsConnectBackendApplication.java
+++ src/main/java/com/connect/acts/ActsConnectBackend/ActsConnectBackendApplication.java
@@ -1,5 +1,6 @@
 package com.connect.acts.ActsConnectBackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ActsConnectBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		// set environment variables for prod
+		System.setProperty("PROD_DB_URL", dotenv.get("PROD_DB_URL"));
+		System.setProperty("PROD_DB_UNAME", dotenv.get("PROD_DB_UNAME"));
+		System.setProperty("PROD_DB_PWD", dotenv.get("PROD_DB_PWD"));
+
+		// set jwt env variables
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("JWT_EXPIRATION_TIME", dotenv.get("JWT_EXPIRY"));
+
+		// set environment variables for dev
+		System.setProperty("DEV_DB_URL", dotenv.get("DEV_DB_URL"));
+		System.setProperty("DEV_DB_UNAME", dotenv.get("DEV_DB_UNAME"));
+		System.setProperty("DEV_DB_PWD", dotenv.get("DEV_DB_PWD"));
 		SpringApplication.run(ActsConnectBackendApplication.class, args);
 	}
 }
